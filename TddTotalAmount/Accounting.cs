e79d001affd79dec940bc882ae9afcb262ff2f4f
urbrioche <aafulin@gmail.com>
2018-03-21 21:29:37 +0800

    use Sum() instead of foreach aggregate

--- TddTotalAmount/Accounting.cs
+++ TddTotalAmount/Accounting.cs
@@ -16,20 +16,7 @@ namespace TddTotalAmount
         {
             var period = new Period(startDate, endDate);
             var budgets = _repository.GetAll();
-            var totalAmount = 0m;
-            foreach (var budget in budgets)
-            {
-                totalAmount += budget.EffectiveAmount(period);
-            }
-
-            return totalAmount;
-            if (budgets.Any())
-            {
-                var budget = budgets[0];
-                return budget.EffectiveAmount(period);
-            }
-
-            return 0;
+            return budgets.Sum(b => b.EffectiveAmount(period));
         }
     }
 }
