f5270e2498d371c60a7cad355098dd7d3e9de739
urbrioche <aafulin@gmail.com>
2018-03-21 21:08:14 +0800

    pass test: invalid period

--- TddTotalAmount/Period.cs
+++ TddTotalAmount/Period.cs
@@ -9,6 +9,10 @@ namespace TddTotalAmount
 
         public Period(DateTime startDate, DateTime endDate)
         {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException();
+            }
             StartDate = startDate;
             EndDate = endDate;
         }
--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -62,6 +62,14 @@ namespace TddTotalAmount
             TotalAmountShouldBe(1, new DateTime(2018, 4, 30), new DateTime(2018, 5, 1));
         }
 
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Invalid_Period()
+        {
+            GivenBudgets(new Budget { YearMonth = "201804", Amount = 30 });
+            TotalAmountShouldBe(1, new DateTime(2018, 6, 30), new DateTime(2018, 5, 1));
+        }
+
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
         {
             Assert.AreEqual(expected, _accounting.TotalAmount(startDate, endDate));
