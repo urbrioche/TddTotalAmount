dc425a37339ee1ad82e763fc479668e70b968f96
urbrioche <aafulin@gmail.com>
2018-04-06 19:54:02 +0800

    pass test: No_Budgets

---
+++ TddTotalAmount/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TddTotalAmount
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+    }
+
+    public class Accounting
+    {
+        private readonly IRepository<Budget> _repository;
+
+        public Accounting(IRepository<Budget> repository)
+        {
+            _repository = repository;
+        }
+
+        public decimal TotalAmount(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+}
