9b18389b98b5119bcbedbddde119f86d72291a8a
urbrioche <aafulin@gmail.com>
2018-04-28 14:11:00 +0800

    pass tet no budget

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace TddTotalAmount
 {
@@ -6,8 +9,13 @@ namespace TddTotalAmount
     public class TotalAmountTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void no_budget()
         {
+            var repository = Substitute.For<IRepository<Budget>>();
+            repository.GetAll().Returns(new List<Budget>());
+            var accounting = new Accounting(repository);
+            var totalAmount = accounting.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+            Assert.AreEqual(0, totalAmount);
         }
     }
 }
