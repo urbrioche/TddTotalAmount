a182d4811e844c1309569a53ac29951f2e2c820a
urbrioche <aafulin@gmail.com>
2018-04-29 13:37:51 +0800

    pass test: no budget

---
+++ TddTotalAmount/Accounting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace TddTotalAmount
+{
+    public class Accounting
+    {
+        private IRepository<Budget> _repo;
+
+        public Accounting(IRepository<Budget> repo)
+        {
+            this._repo = repo;
+        }
+
+        public decimal TotalAmount(DateTime startDate, DateTime endDate)
+        {
+            var budgets = _repo.GetAll();
+            return 0;
+        }
+    }
+}
