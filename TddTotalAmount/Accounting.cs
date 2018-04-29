89c5fa0bb7a177d32488a891ef6f6fc1aec398d2
urbrioche <aafulin@gmail.com>
2018-04-29 14:19:23 +0800

    move period into new file

--- TddTotalAmount/Accounting.cs
+++ TddTotalAmount/Accounting.cs
@@ -3,36 +3,6 @@ using System.Linq;
 
 namespace TddTotalAmount
 {
-    public class Period
-    {
-        public Period(DateTime startDate, DateTime endDate)
-        {
-            StartDate = startDate;
-            EndDate = endDate;
-        }
-
-        public DateTime StartDate { get; private set; }
-        public DateTime EndDate { get; private set; }
-
-        public int OverlappingDays(Period period)
-        {
-            if (EndDate < period.StartDate)
-            {
-                return 0;
-            }
-
-            if (StartDate > period.EndDate)
-            {
-                return 0;
-            }
-
-            var effectiveEndDate = EndDate > period.EndDate ? period.EndDate: EndDate;
-
-            var effectiveStartDate = StartDate < period.StartDate ? period.StartDate : StartDate;
-            return (effectiveEndDate.AddDays(1) - effectiveStartDate).Days;
-        }
-    }
-
     public class Accounting
     {
         private IRepository<Budget> _repo;
