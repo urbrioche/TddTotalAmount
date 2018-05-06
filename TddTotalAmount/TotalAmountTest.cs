155326f42ad3dac4577a421a5adb5e85b445b9c6
urbrioche <aafulin@gmail.com>
2018-05-06 17:16:12 +0800

    pass test: one effective day, period overlap budget month LastDay

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -43,6 +43,14 @@ namespace TddTotalAmount
         }
 
         [TestMethod]
+        public void no_effective_day_period_after_budget_month()
+        {
+            GivenBudgets(new Budget() { YearMonth = "201804", Amount = 30 });
+
+            TotalAmountShouldBe(0, new DateTime(2018, 5, 1), new DateTime(2018, 5, 1));
+        }
+
+        [TestMethod]
         public void one_effective_day_period_overlap_budget_month_FirstDay()
         {
             GivenBudgets(new Budget() { YearMonth = "201804", Amount = 30 });
@@ -51,11 +59,11 @@ namespace TddTotalAmount
         }
 
         [TestMethod]
-        public void no_effective_day_period_after_budget_month()
+        public void one_effective_day_period_overlap_budget_month_LastDay()
         {
             GivenBudgets(new Budget() { YearMonth = "201804", Amount = 30 });
 
-            TotalAmountShouldBe(0, new DateTime(2018, 5, 1), new DateTime(2018, 5, 1));
+            TotalAmountShouldBe(1, new DateTime(2018, 4, 30), new DateTime(2018, 5, 1));
         }
 
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
