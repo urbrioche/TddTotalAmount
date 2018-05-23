12a222cf5e58151fa69620bc5974ca80acb92042
urbrioche <aafulin@gmail.com>
2018-05-23 19:36:43 +0800

    pass test: no budgets

---
+++ TddTotalAmount/TotalAmountTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TddTotalAmount
+{
+    [TestClass]
+    public class TotalAmountTest
+    {
+        [TestMethod]
+        public void no_budgets()
+        {
+            var repository = Substitute.For<IRepository<Budget>>();
+            var accounting = new Accounting(repository);
+            var totalAmount = accounting.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+            Assert.AreEqual(totalAmount, 0);
+        }
+    }
+}
