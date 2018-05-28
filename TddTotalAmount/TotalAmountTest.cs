1c9635a50a7e6ac0e5a62681aa2bb4f45d252acd
urbrioche <aafulin@gmail.com>
2018-05-28 19:01:46 +0800

    pass one effective day period inside budget month

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -25,6 +25,13 @@ namespace TddTotalAmount
             TotalAmountShouldBe(0, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
         }
 
+        [TestMethod]
+        public void one_effective_day_period_inside_budget_month()
+        {
+            GivenBudgets(new Budget(){ YearMonth = "201804", Amount = 30});
+            TotalAmountShouldBe(1, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+        }
+
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
         {
             Assert.AreEqual(expected, _accounting.TotalAmount(startDate, endDate));
