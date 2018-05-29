114b12fc44ff5461295aef26ca295ef7c080d339
urbrioche <aafulin@gmail.com>
2018-05-29 20:37:27 +0800

    extract Period class

---
+++ TddTotalAmount/Period.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TddTotalAmount
+{
+    public class Period
+    {
+        public Period(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+    }
+}
