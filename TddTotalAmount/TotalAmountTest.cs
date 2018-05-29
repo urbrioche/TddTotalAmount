2ec58887e1bc4e602559b3ee8e953b128e7a561c
urbrioche <aafulin@gmail.com>
2018-05-29 20:13:12 +0800

    no budgets

---
+++ TddTotalAmount/TotalAmountTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+
+namespace TddTotalAmount
+{
+    [TestClass]
+    public class TotalAmountTest
+    {
+        [TestMethod]
+        public void no_budgets()
+        {
+            var repository = Substitute.For<IRepository<Budget>>();
+            repository.GetAll().Returns(new List<Budget>());
+            var accounting = new Accounting(repository);
+            var totalAmount = accounting.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+            Assert.AreEqual(0, totalAmount);
+        }
+    }
+
+    public class Accounting
+    {
+        private readonly IRepository<Budget> _repository;
+
+        public Accounting(IRepository<Budget> repository)
+        {
+            _repository = repository;
+        }
+
+        public decimal TotalAmount(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+
+    public class Budget
+    {
+    }
+}
