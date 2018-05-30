cfa0d48d1ccf455ada9b1f3cb9544d07b69a9d64
urbrioche <aafulin@gmail.com>
2018-05-30 20:57:51 +0800

    refactor Period class

--- TddTotalAmount/Period.cs
+++ TddTotalAmount/Period.cs
@@ -19,16 +19,27 @@ namespace TddTotalAmount
 
         public int OverlappingDays(Period period)
         {
-            if (EndDate < period.StartDate || StartDate > period.EndDate)
+            if (HasNoOverlapping(period))
             {
                 return 0;
             }
 
-            var effectiveEndDate = EndDate > period.EndDate ? period.EndDate : EndDate;
-            var effectiveStartDate = StartDate < period.StartDate ? period.StartDate : StartDate;
+            return (EffectiveEndDate(period).AddDays(1) - EffectiveStartDate(period)).Days;
+        }
 
-            var days = (effectiveEndDate.AddDays(1) - effectiveStartDate).Days;
-            return days;
+        private DateTime EffectiveStartDate(Period period)
+        {
+            return StartDate < period.StartDate ? period.StartDate : StartDate;
+        }
+
+        private DateTime EffectiveEndDate(Period period)
+        {
+            return EndDate > period.EndDate ? period.EndDate : EndDate;
+        }
+
+        private bool HasNoOverlapping(Period period)
+        {
+            return EndDate < period.StartDate || StartDate > period.EndDate;
         }
     }
 }
