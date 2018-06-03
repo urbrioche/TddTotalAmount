162e4d3e73081f6bb34cc182071534f68750057f
林建甫 <aafulin@gmail.com>
2018-06-03 19:56:06 +0800

    pass test: no budgets

---
+++ TddTotalAmount/Accounting.cs
@@ -0,0 +1,17 @@
+﻿namespace TddTotalAmount
+{
+    public class Accounting
+    {
+        private readonly IRepository<Budget> _repository;
+
+        public Accounting(IRepository<Budget> repository)
+        {
+            _repository = repository;
+        }
+
+        public decimal TotalAmount()
+        {
+            return 0;
+        }
+    }
+}
---
+++ TddTotalAmount/TotalAmountTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections.Generic;
+
+namespace TddTotalAmount
+{
+    [TestClass]
+    public class TotalAmountTest
+    {
+        [TestMethod]
+        public void no_budgets()
+        {
+            var repository = Substitute.For<IRepository<Budget>>();
+            repository.GetAll().Returns(new List<Budget>());
+            var accounting = new Accounting(repository);
+            var totalAmount = accounting.TotalAmount();
+            Assert.AreEqual(0, totalAmount);
+        }
+    }
+}
