81046a980a3fa82de883a868cdbae17fddebbc2c
林建甫 <aafulin@gmail.com>
2018-06-04 21:33:33 +0800

    pass test: no effective days, period after budget month

--- TddTotalAmount/Budget.cs
+++ TddTotalAmount/Budget.cs
@@ -11,5 +11,14 @@ namespace TddTotalAmount
         {
             get { return DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null); }
         }
+
+        public DateTime LastDay
+        {
+            get
+            {
+                var daysInMonth = DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month);
+                return DateTime.ParseExact(YearMonth + daysInMonth, "yyyyMMdd", null);
+            }
+        }
     }
 }
