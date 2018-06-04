6fa0ed4b4c3c4bd82f59b3cd41d8cd30984eefc5
林建甫 <aafulin@gmail.com>
2018-06-04 21:08:25 +0800

    pass test: no budgets

---
+++ TddTotalAmount/Accounting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TddTotalAmount
+{
+    public class Accounting
+    {
+        public decimal TotalAmount(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+}
