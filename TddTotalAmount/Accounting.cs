ec1ac29dc755a816733268029009a26c31458a70
林建甫 <aafulin@gmail.com>
2018-06-04 21:18:22 +0800

    pass test: one effective day, period inside budget month

--- TddTotalAmount/Accounting.cs
+++ TddTotalAmount/Accounting.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace TddTotalAmount
 {
     public class Accounting
     {
+        private readonly IRepository<Budget> _repository;
+
+        public Accounting(IRepository<Budget> repository)
+        {
+            _repository = repository;
+        }
+
         public decimal TotalAmount(DateTime startDate, DateTime endDate)
         {
+            var budgets = _repository.GetAll();
+            if (budgets.Any())
+            {
+                var days = (endDate.AddDays(1) - startDate).Days;
+                return days;
+            }
+
             return 0;
         }
     }
