adf02a31d80a84d56024373a835295693ca418c2
林建甫 <aafulin@gmail.com>
2018-06-04 21:54:02 +0800

    extract method EffectiveAmount and move into Budget class

--- TddTotalAmount/Accounting.cs
+++ TddTotalAmount/Accounting.cs
@@ -19,9 +19,7 @@ namespace TddTotalAmount
             if (budgets.Any())
             {
                 var budget = budgets[0];
-                var dailyAmount = budget.DailyAmount();
-                var days = period.OverlappingDays(new Period(budget.FirstDay, budget.LastDay));
-                return days * dailyAmount;
+                return budget.EffectiveAmount(period);
             }
 
             return 0;
