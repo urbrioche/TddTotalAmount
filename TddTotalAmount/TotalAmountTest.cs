b0c88adb0bad298378c142eeb95857b6637b1f1f
Jerry Lin 林建甫 (jerrylin) <jerrylin@micron.com>
2018-06-05 21:34:47 +0800

    pass

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -32,6 +32,13 @@ namespace TddTotalAmount
             TotalAmountShouldBe(1, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
         }
 
+        [TestMethod]
+        public void no_effective_day_period_before_budget_month()
+        {
+            GivenBudgets(new Budget() {YearMonth = "201804", Amount = 30});
+            TotalAmountShouldBe(0, new DateTime(2018, 3, 31), new DateTime(2018, 3, 31));
+        }
+
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
         {
             var totalAmount = _accounting.TotalAmount(startDate, endDate);
