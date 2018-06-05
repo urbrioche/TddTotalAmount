ebe76a03208ee23661c268bfb2556e1ace623d33
Jerry Lin 林建甫 (jerrylin) <jerrylin@micron.com>
2018-06-05 21:59:15 +0800

    refactor Budget

--- TddTotalAmount/Budget.cs
+++ TddTotalAmount/Budget.cs
@@ -5,44 +5,30 @@ namespace TddTotalAmount
     public class Budget
     {
         public int Amount { get; set; }
-        public string YearMonth { get; set; }
-
         public DateTime FirstDay
         {
-            get
-            {
-                return DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null);
-            }
+            get { return DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null); }
         }
 
         public DateTime LastDay
         {
-            get
-            {
-                var daysInMonth = DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month);
-                return DateTime.ParseExact(YearMonth + daysInMonth, "yyyyMMdd", null);
-            }
+            get { return DateTime.ParseExact(YearMonth + TotalDays, "yyyyMMdd", null); }
         }
 
-        public int TotalDays
+        public string YearMonth { get; set; }
+        private int TotalDays
         {
-            get
-            {
-                return DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month);
-            }
-        }
-
-        public int DailyAmount()
-        {
-            var dailyAmount = Amount / TotalDays;
-            return dailyAmount;
+            get { return DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month); }
         }
 
         public decimal EffectiveAmount(Period period)
         {
-            var dailyAmount = DailyAmount();
-            var days = period.OverlappingDays(new Period(FirstDay, LastDay));
-            return days * dailyAmount;
+            return period.OverlappingDays(new Period(FirstDay, LastDay)) * DailyAmount();
+        }
+
+        private int DailyAmount()
+        {
+            return Amount / TotalDays;
         }
     }
 }
