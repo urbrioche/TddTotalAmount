91ff781869fa765f83e1b9850580fe4a6a6a65d8
林建甫 <aafulin@gmail.com>
2018-06-08 20:26:56 +0800

    pass

---
+++ TddTotalAmount/Accounting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TddTotalAmount
+{
+    public class Accounting
+    {
+        private readonly IRepository<Budget> _repository;
+
+        public Accounting(IRepository<Budget> repository)
+        {
+            _repository = repository;
+        }
+
+        public decimal TotalAmount(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+}
