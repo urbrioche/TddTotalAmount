fba80a45dd77e418f34fbce499fa82b6b764da09
林建甫 <aafulin@gmail.com>
2018-06-11 20:05:19 +0800

    pass

--- TddTotalAmount/Period.cs
+++ TddTotalAmount/Period.cs
@@ -4,17 +4,22 @@ namespace TddTotalAmount
 {
     public class Period
     {
-        public DateTime StartDate { get; }
-        public DateTime EndDate { get; }
-
         public Period(DateTime startDate, DateTime endDate)
         {
             StartDate = startDate;
             EndDate = endDate;
         }
 
-        public int EffectiveDays()
+        public DateTime EndDate { get; }
+        public DateTime StartDate { get; }
+
+        public int EffectiveDays(Budget budget)
         {
+            if (EndDate < budget.FirstDay)
+            {
+                return 0;
+            }
+
             var days = (EndDate.AddDays(1) - StartDate).Days;
             return days;
         }
