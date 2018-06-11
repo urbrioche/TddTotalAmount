ea3f402799e1c338112ad541489025a937666ab0
林建甫 <aafulin@gmail.com>
2018-06-11 20:00:23 +0800

    pass

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 
@@ -24,15 +25,22 @@ namespace TddTotalAmount
             TotalAmountShouldBe(0, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
         }
 
+        [TestMethod]
+        public void one_effective_day_period_inside_budget_month()
+        {
+            GivenBudgets(new Budget() { YearMonth = "201804", Amount = 30 });
+            TotalAmountShouldBe(1, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+        }
+
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
         {
             var totalAmount = _accounting.TotalAmount(startDate, endDate);
             Assert.AreEqual(expected, totalAmount);
         }
 
-        private void GivenBudgets()
+        private void GivenBudgets(params Budget[] budgets)
         {
-            _repository.GetAll().Returns(new List<Budget>());
+            _repository.GetAll().Returns(budgets.ToList());
         }
     }
 }
