ebbbf2b9f3069b22ab19dd6181d7302f3035c9e6
林建甫 <aafulin@gmail.com>
2018-06-14 20:38:17 +0800

    refactor

--- TddTotalAmount/Period.cs
+++ TddTotalAmount/Period.cs
@@ -15,13 +15,18 @@ namespace TddTotalAmount
 
         public int EffectiveDays(Budget budget)
         {
-            if (EndDate < budget.FirstDay || StartDate > budget.LastDay)
+            if (HasNoOverlap(budget))
             {
                 return 0;
             }
 
             var days = (EndDate.AddDays(1) - StartDate).Days;
             return days;
+        }
+
+        private bool HasNoOverlap(Budget budget)
+        {
+            return EndDate < budget.FirstDay || StartDate > budget.LastDay;
         }
     }
 }
