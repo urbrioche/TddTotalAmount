9b791821d9d2e6b0eeb538a6d3059d98948f4c14
林建甫 <aafulin@gmail.com>
2018-06-16 23:37:34 +0800

    pass

--- TddTotalAmount/Period.cs
+++ TddTotalAmount/Period.cs
@@ -19,7 +19,10 @@ namespace TddTotalAmount
             {
                 return 0;
             }
-            var days = (EndDate.AddDays(1) - StartDate).Days;
+
+            var effectiveEndDate = EndDate;
+            var effectiveStartDate = StartDate < budget.FirstDay ? budget.FirstDay : StartDate;
+            var days = (effectiveEndDate.AddDays(1) - effectiveStartDate).Days;
             return days;
         }
     }
