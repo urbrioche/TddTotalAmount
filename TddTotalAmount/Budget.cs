c1ff7542b7e732ec4d571d238483f85c699347c9
林建甫 <aafulin@gmail.com>
2018-06-16 23:34:59 +0800

    pass

--- TddTotalAmount/Budget.cs
+++ TddTotalAmount/Budget.cs
@@ -11,5 +11,14 @@ namespace TddTotalAmount
         {
             get { return DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null); }
         }
+
+        public DateTime LastDay
+        {
+            get
+            {
+                var totalDays = DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month);
+                return DateTime.ParseExact(YearMonth + totalDays, "yyyyMMdd", null);
+            }
+        }
     }
 }
