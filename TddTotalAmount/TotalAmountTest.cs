b8d4bcf808c566eaf5488de3fd40df3a5c87514d
林建甫 <aafulin@gmail.com>
2018-06-18 12:58:38 +0800

    refactor

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 
@@ -8,14 +9,30 @@ namespace TddTotalAmount
     [TestClass]
     public class TotalAmountTest
     {
+        private IRepository<Budget> _repository = Substitute.For<IRepository<Budget>>();
+        private Accounting _accounting;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _accounting = new Accounting(_repository);
+        }
+
         [TestMethod]
         public void no_budgets()
         {
-            var repository = Substitute.For<IRepository<Budget>>();
-            repository.GetAll().Returns(new List<Budget>());
-            var accounting = new Accounting(repository);
-            var totalAmount = accounting.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
-            Assert.AreEqual(0, totalAmount);
+            GivenBudgets();
+            TotalAmountShouldBe(0, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+        }
+
+        private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
+        {
+            Assert.AreEqual(expected, _accounting.TotalAmount(startDate, endDate));
+        }
+
+        private void GivenBudgets(params Budget[] expected)
+        {
+            _repository.GetAll().Returns(expected.ToList());
         }
     }
 }
