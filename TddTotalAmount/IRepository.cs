b9312af24ad8d855376e833f524b827b2914b325
林建甫 <aafulin@gmail.com>
2018-06-20 20:23:20 +0800

    refactor

---
+++ TddTotalAmount/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TddTotalAmount
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+    }
+}
---
+++ TddTotalAmount/TotalAmountTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TddTotalAmount
+{
+    [TestClass]
+    public class TotalAmountTest
+    {
+        private IRepository<Budget> _repository = Substitute.For<IRepository<Budget>>();
+        private Accounting _accounting;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _accounting = new Accounting(_repository);
+        }
+
+        [TestMethod]
+        public void no_budgets()
+        {
+            GivenBudgets();
+            TotalAmountShouldBe(0, new DateTime(2018, 4, 1), new DateTime(2018, 4, 1));
+        }
+
+        private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
+        {
+            var totalAmount = _accounting.TotalAmount(startDate, endDate);
+            Assert.AreEqual(expected, totalAmount);
+        }
+
+        private void GivenBudgets(params Budget[] budgets)
+        {
+            _repository.GetAll().Returns(budgets.ToList());
+        }
+    }
+}
