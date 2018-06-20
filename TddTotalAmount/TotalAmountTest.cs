be456a8beff8e3c1431ba817b3f0608236cc0a70
林建甫 <aafulin@gmail.com>
2018-06-20 20:47:05 +0800

    pass

--- TddTotalAmount/TotalAmountTest.cs
+++ TddTotalAmount/TotalAmountTest.cs
@@ -67,6 +67,14 @@ namespace TddTotalAmount
             TotalAmountShouldBe(20, new DateTime(2018, 4, 1), new DateTime(2018, 4, 2));
         }
 
+        [TestMethod]
+        public void multiple_budgets()
+        {
+            GivenBudgets(new Budget() { YearMonth = "201804", Amount = 300 },
+                new Budget() { YearMonth = "201806", Amount = 30 });
+            TotalAmountShouldBe(103, new DateTime(2018, 4, 21), new DateTime(2018, 6, 3));
+        }
+
         private void TotalAmountShouldBe(int expected, DateTime startDate, DateTime endDate)
         {
             var totalAmount = _accounting.TotalAmount(startDate, endDate);
