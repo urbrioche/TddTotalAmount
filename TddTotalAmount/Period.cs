8715df9c0ed4d5dc0e0bd9e8eddbcaa918b7c7b2
林建甫 <aafulin@gmail.com>
2018-06-24 18:50:37 +0800

    refactor

---
+++ TddTotalAmount/Period.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TddTotalAmount
+{
+    public class Period
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+
+        public Period(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public int EffectiveDays()
+        {
+            return (EndDate.AddDays(1) - StartDate).Days;
+        }
+    }
+}
