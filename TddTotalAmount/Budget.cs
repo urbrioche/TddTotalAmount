8b86d6e97b55d74db8e11ea8ded4e56f82e9bd67
林建甫 <aafulin@gmail.com>
2018-06-24 18:53:41 +0800

    pass

--- TddTotalAmount/Budget.cs
+++ TddTotalAmount/Budget.cs
@@ -1,8 +1,18 @@
-﻿namespace TddTotalAmount
+﻿using System;
+
+namespace TddTotalAmount
 {
     public class Budget
     {
         public int Amount { get; set; }
         public string YearMonth { get; set; }
+
+        public DateTime FirstDay
+        {
+            get
+            {
+                return DateTime.ParseExact(YearMonth + "01", "yyyyMMdd", null);
+            }
+        }
     }
 }
